7c6cdd827cc3935e48c5191aef3419f271af5663
amedeomeschini <ameschini2002@gmail.com>
2026-02-20 15:54:42 +0100

    listcounter fix

--- bookreel/src/main/java/it/unipi/bookreel/repository/FilmsNeo4jRepository.java
+++ bookreel/src/main/java/it/unipi/bookreel/repository/FilmsNeo4jRepository.java
@@ -11,29 +11,29 @@ import java.util.List;
 //for each media list, find the 10 most present films
 public interface FilmsNeo4jRepository extends Neo4jRepository<FilmsNeo4j, String> {
     @Query("""
-            MATCH (user:User)-[relationship:LIST_ELEMENT]->(Films:Films)
-            WITH Films, relationship,
-                 CASE
-                     WHEN relationship.progress = 1 THEN 'COMPLETED'
-                     ELSE 'PLANNED'
-                 END AS listType
-            WITH  Films, listType, count(DISTINCT relationship) AS listCount
-            ORDER BY listType, listCount DESC
-            WITH listType, collect({id: Films.id, name: Films.name, count: listCount})[0..10] AS topMedia
-            RETURN listType, topMedia
-            """)
+        MATCH (user:User)-[relationship:LIST_ELEMENT]->(f:Films)
+        WITH f, relationship,
+             CASE
+                 WHEN relationship.progress = 1 THEN 'COMPLETED'
+                 ELSE 'PLANNED'
+             END AS listType
+        WITH f, listType, count(DISTINCT relationship) AS listCount
+        ORDER BY listType, listCount DESC
+        WITH listType, collect({id: f.id, name: f.name, listCount: listCount})[0..10] AS topMedia
+        RETURN listType, topMedia
+        """)
     List<ListCounterAnalyticDto> findListCounters();
 
     @Query("""
-            MATCH (user:User)-[relationship:LIST_ELEMENT]->(Films:Films {id: $FilmsId})
-            WITH Films, relationship,
-                 CASE
-                     WHEN relationship.progress = 1 THEN 'COMPLETED'
-                     ELSE 'PLANNED'
-                 END AS listType
-            WITH  Films, listType, count(DISTINCT relationship) AS listCount
-            WITH Films, collect({listType: listType, listCount: listCount}) AS appearances
-            RETURN Films.id AS mediaId, Films.name AS mediaName, appearances
-            """)
-    List<MediaInListsAnalyticDto> findFilmsAppearancesInLists(String FilmsId);
+        MATCH (user:User)-[relationship:LIST_ELEMENT]->(f:Films {id: $filmsId})
+        WITH f, relationship,
+             CASE
+                 WHEN relationship.progress = 1 THEN 'COMPLETED'
+                 ELSE 'PLANNED'
+             END AS listType
+        WITH f, listType, count(DISTINCT relationship) AS listCount
+        WITH f, collect({listType: listType, listCount: listCount}) AS appearances
+        RETURN f.id AS mediaId, f.name AS mediaName, appearances
+        """)
+    List<MediaInListsAnalyticDto> findFilmsAppearancesInLists(String filmsId);
 }
